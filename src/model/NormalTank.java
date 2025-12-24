9c3ef0dc51b899b3ce3b6547e6c441066730869d
miksa0077 <3069725839@qq.com>
2025-12-24 21:21:21 +0800

    Create NormalTank.java

---
+++ src/model/NormalTank.java
@@ -0,0 +1,36 @@
+package model;
+
+import infra.GameConfig;
+import javafx.scene.image.Image;
+
+public class NormalTank extends Tank {
+
+    public NormalTank(double x, double y) {
+        super(x, y,
+                TankType.ENEMY_NORMAL,
+                GameConfig.TANK_SPEED * GameConfig.NORMAL_SPEED_MULTIPLIER,
+                GameConfig.TANK_ROTATION_SPEED * GameConfig.NORMAL_ROTATION_MULTIPLIER,
+                GameConfig.NORMAL_HEALTH,
+                GameConfig.NORMAL_FIRE_COOLDOWN,
+                GameConfig.NORMAL_BULLET_DAMAGE,
+                GameConfig.BULLET_SPEED,
+                GameConfig.NORMAL_SCORE_VALUE);
+
+        setSmoothFactor(0.2); // 普通坦克中等灵敏度
+    }
+
+    @Override
+    protected void loadImage() {
+        try {
+            this.imagePath = GameConfig.IMAGE_PATH + "tank_yellow.png";
+            this.tankImage = new Image(this.imagePath);
+        } catch (Exception e) {
+            System.err.println("普通坦克图片加载失败");
+        }
+    }
+
+    @Override
+    public String getColorDescription() {
+        return "黄色普通坦克";
+    }
+}
