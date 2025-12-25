70fe485428102b9bf2708c54f4f944e7cf4ae0bd
miksa0077 <3069725839@qq.com>
2025-12-25 11:14:48 +0800

    Update NormalTank.java

--- src/model/NormalTank.java
+++ src/model/NormalTank.java
@@ -3,6 +3,9 @@ package model;
 import infra.GameConfig;
 import javafx.scene.image.Image;
 
+/**
+ * 敌人黄色普通坦克
+ */
 public class NormalTank extends Tank {
 
     public NormalTank(double x, double y) {
@@ -16,7 +19,8 @@ public class NormalTank extends Tank {
                 GameConfig.BULLET_SPEED,
                 GameConfig.NORMAL_SCORE_VALUE);
 
-        setSmoothFactor(0.2); // 普通坦克中等灵敏度
+        // 普通坦克中等灵敏度
+        setRotationSmoothness(0.2);
     }
 
     @Override
@@ -24,8 +28,13 @@ public class NormalTank extends Tank {
         try {
             this.imagePath = GameConfig.IMAGE_PATH + "tank_yellow.png";
             this.tankImage = new Image(this.imagePath);
+            if (tankImage.isError()) {
+                System.err.println("普通坦克图片加载失败: " + imagePath);
+                this.tankImage = null;
+            }
         } catch (Exception e) {
-            System.err.println("普通坦克图片加载失败");
+            System.err.println("普通坦克图片加载异常: " + e.getMessage());
+            this.tankImage = null;
         }
     }
 
